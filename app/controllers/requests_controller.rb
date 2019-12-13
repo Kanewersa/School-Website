921561047c753574515951f1c1cd0872e0407dde
Kanewersa <kanewers@gmail.com>
2019-12-13 16:09:48 +0100

    Added basic request accept/reject logic

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,32 @@
 class RequestsController < ApplicationController
   layout 'dashboard'
+
+  def accept
+    @request = Request.find(params[:request][:id])
+    @obj = find_request_object(@request)
+
+    case @request.action
+    when "create"
+    when "destroy"
+      @obj.destroy
+    else
+      @source = @request[:requestable_type].classify.constantize.find(@request[:requestable_id])
+      @obj.update_attributes(@source.attributes)
+      @source.attachments.each do |attachment|
+        @obj.attach(attachment)
+      end
+    end
+
+    if @obj != NIL
+      @obj.status = 1
+    end
+    @request.status = 2
+  end
+
+  def reject
+
+  end
+
   def show
 
   end
