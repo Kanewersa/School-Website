d4386c2ca31340fb497a8b54aafd837a856de63e
Kanewersa <kanewers@gmail.com>
2019-12-13 16:38:10 +0100

    Updated requests display

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -11,7 +11,7 @@ module RequestsHelper
     end
   end
 
-  def resource_type(type)
+  def resource_type_name(type)
     case type
     when "Event"
       "wydarzenie"
@@ -20,8 +20,17 @@ module RequestsHelper
     end
   end
 
-  def resource_name(name)
-
+  def status_name(status)
+    case status
+    when "canceled"
+      "<i class='fas fa-pause' data-toggle='tooltip' title='Anulowałeś swoje zgłoszenie'> Anulowano</i>".html_safe
+    when "pending"
+      "<i class='fas fa-hourglass-half' data-toggle='tooltip' title='Zgłoszenie oczekuje na rozpatrzenie przez administratora'> W trakcie</i>".html_safe
+    when "approved"
+      "<i class='fas fa-check' data-toggle='tooltip' title='Zgłoszenie zostało rozpatrzone pozytywnie'> Zaakceptowano</i>".html_safe
+    else
+      "<i class='fas fa-times' data-toggle='tooltip' title='Zgłoszenie zostało odrzucone'> Odrzucono</i>".html_safe
+    end
   end
 
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -3,4 +3,5 @@ class Request < ApplicationRecord
   belongs_to :requestable, :polymorphic => true, :dependent => :destroy
 
   enum status: { canceled: 0, pending: 1, approved: 2, rejected: 3 }
+
 end
