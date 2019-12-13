c50f3df9174e44bf31c3832694b967457721d166
Kanewersa <kanewers@gmail.com>
2019-12-13 14:37:00 +0100

    Fixed finding request object in case of 'edit' action

--- app/controllers/panels_controller.rb
+++ app/controllers/panels_controller.rb
@@ -11,7 +11,12 @@ class PanelsController < ApplicationController
 
   helper_method :find_request_object
   def find_request_object(request)
-    request[:requestable_type].classify.constantize.find(request[:requestable_id])
+    id = request[:requestable_id]
+    if request[:action].start_with?("edit")
+      text = request[:action].split("/")
+      id = text[1]
+    end
+    request[:requestable_type].classify.constantize.find(id)
   end
 
   def check_if_admin
