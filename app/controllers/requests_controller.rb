4033bd200cd4e74766bcdda947a41a8ee36110b0
Kanewersa <kanewers@gmail.com>
2019-12-13 18:45:55 +0100

    Fixed requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,16 +2,17 @@ class RequestsController < ApplicationController
   layout 'dashboard'
 
   def accept
-    @request = Request.find(params[:request][:id])
+    @request = Request.find(params[:id])
     @obj = find_request_object(@request)
 
-    case @request.action
+    case @request[:action]
     when "create"
     when "destroy"
       @obj.destroy
     else
       @source = @request[:requestable_type].classify.constantize.find(@request[:requestable_id])
       @obj.update_attributes(@source.attributes)
+      @obj.purge
       @source.attachments.each do |attachment|
         @obj.attach(attachment)
       end
@@ -21,10 +22,15 @@ class RequestsController < ApplicationController
       @obj.status = 1
     end
     @request.status = 2
+    @obj.save
+    @request.save
+    redirect_to requests_path
   end
 
   def reject
-
+    @request = Request.find(params[:id])
+    @request.status = 3
+    @request.save
   end
 
   def show
@@ -32,6 +38,6 @@ class RequestsController < ApplicationController
   end
 
   private def request_params
-    params.require(:request)
+    params.require(:request).permit(:id)
   end
 end
