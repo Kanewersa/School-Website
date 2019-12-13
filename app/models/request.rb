379289cb491d7b2a1f9dc24d69f2540ff545188e
Kanewersa <kanewers@gmail.com>
2019-12-13 12:35:40 +0100

    Destroy request when user or requestable gets deleted

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,6 @@
 class Request < ApplicationRecord
-  belongs_to :user
-  belongs_to :requestable, :polymorphic => true
+  belongs_to :user, :dependent => :destroy
+  belongs_to :requestable, :polymorphic => true, :dependent => :destroy
 
   enum status: { canceled: 0, pending: 1, approved: 2, rejected: 3 }
 end
