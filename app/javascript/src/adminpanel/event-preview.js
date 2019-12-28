4d913178fd8623dc6c117112e5428ca663a29460
Jacob <kanewers@gmail.com>
2019-12-28 13:18:04 +0100

    Changed preview to allow previewing different objects

--- app/javascript/src/adminpanel/event-preview.js
+++ app/javascript/src/adminpanel/event-preview.js
@@ -2,5 +2,7 @@ $('form').on('ajax:success', function(event, data, status, xhr) {
     $('#preview-modal').modal('show');
 });
 $('form').on('ajax:error', function(event, data, status, xhr) {
-    location.reload();
+    console.log("There was an error running ajax request, here is the 'data':");
+    console.log(data);
+    // location.reload();
 });
