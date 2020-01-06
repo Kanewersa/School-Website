6fd608443ccbecc82e5df0405db8bf4af684aadf
Jacob <kanewers@gmail.com>
2020-01-06 22:18:46 +0100

    Move url helper for gallery to the requestables helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,22 +1,2 @@
 module EventsHelper
-
-  def event_image_url
-    url_for(@event.image)
-  end
-
-  def gallery_images_urls
-    urls = []
-    @event.gallery_images.each do |image|
-      urls.push(url_for(image))
-    end
-    urls
-  end
-
-  def gallery_images_signed_ids
-    ids = []
-    @event.gallery_images.blobs.each do |blob|
-      ids.push(blob.signed_id)
-    end
-    ids
-  end
 end
--- app/helpers/requestables_helper.rb
+++ app/helpers/requestables_helper.rb
@@ -1,2 +1,25 @@
 module RequestablesHelper
+  def main_image_url(requestable)
+    if requestable.image
+      url_for(requestable.image)
+    elsif requestable.main_tab
+      url_for(requestable.main_tab.image)
+    end
+  end
+
+  def gallery_images_urls(requestable)
+    urls = []
+    requestable.gallery_images.each do |image|
+      urls.push(url_for(image))
+    end
+    urls
+  end
+
+  def gallery_images_signed_ids(requestable)
+    ids = []
+    requestable.gallery_images.blobs.each do |blob|
+      ids.push(blob.signed_id)
+    end
+    ids
+  end
 end
