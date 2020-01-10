10e6684367b78560f2845b167a6d63baf5f368bb
Jacob <kanewers@gmail.com>
2020-01-10 17:04:31 +0100

    Fixed error display

--- app/javascript/src/adminpanel/requestable-ajax.js
+++ app/javascript/src/adminpanel/requestable-ajax.js
@@ -19,10 +19,12 @@ $('form').on('ajax:success', function (event, data, status, xhr) {
 $('form').on('ajax:error', function (event, data, status, xhr) {
     var body = $('#modal-response-body');
     var modal = $('#modal-response');
+    var title = $('#modal-title');
     //Change response modal css for error
     if(modal.hasClass('modal-success')){
         modal.removeClass('modal-success');
         modal.addClass('modal-danger');
+        title.html("Wystąpił błąd");
     }
     //If response was "unprocessable entity"
     if (data.status === 422) {
@@ -31,7 +33,7 @@ $('form').on('ajax:error', function (event, data, status, xhr) {
         //Append errors to modal
         body.empty();
         for (var error of errors) {
-            body.append('<p>' + error +'</p>');
+            body.append(error +'<br>');
         }
         //Show modal
         modal.modal('show');
