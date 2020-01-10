b513d0dbe5999b3c0bfd1a55c4ad64aad08fd427
Jacob <kanewers@gmail.com>
2020-01-10 16:15:02 +0100

    Added basic requestable validation

--- app/javascript/src/adminpanel/requestable-ajax.js
+++ app/javascript/src/adminpanel/requestable-ajax.js
@@ -1,18 +1,42 @@
 var clicked = false;
-$('[id="preview-button"]').click(function(){
+$('[id="preview-button"]').click(function () {
     clicked = true;
 });
 $('#preview-modal').on('hidden.bs.modal', function () {
     clicked = false;
 });
-$('form').on('ajax:success', function(event, data, status, xhr) {
-    if(clicked)
-    {
+$('form').on('ajax:success', function (event, data, status, xhr) {
+    var modal = $('#modal-response');
+    //Change response modal css for success
+    if(modal.hasClass('modal-danger')){
+        modal.removeClass('modal-danger');
+        modal.addClass('modal-success');
+    }
+    if (clicked) {
         $('#preview-modal').modal('show');
     }
 });
-$('form').on('ajax:error', function(event, data, status, xhr) {
-    console.log("There was an error running ajax request, here is the 'data':");
-    console.log(data);
-    // location.reload();
+$('form').on('ajax:error', function (event, data, status, xhr) {
+    var body = $('#modal-response-body');
+    var modal = $('#modal-response');
+    //Change response modal css for error
+    if(modal.hasClass('modal-success')){
+        modal.removeClass('modal-success');
+        modal.addClass('modal-danger');
+    }
+    //If response was "unprocessable entity"
+    if (data.status === 422) {
+                //Render bad inputs
+        var errors = $.parseJSON(data.responseText).errors;
+        //Append errors to modal
+        body.empty();
+        for (var error of errors) {
+            body.append('<p>' + error +'</p>');
+        }
+        //Show modal
+        modal.modal('show');
+    } else {
+        body.empty();
+        body.append('Wystąpił nieoczekiwany błąd o statusie ' + data.status);
+    }
 });
--- app/models/sub_tab.rb
+++ app/models/sub_tab.rb
@@ -4,13 +4,13 @@ class SubTab < ApplicationRecord
   include Requestable
   friendly_id :title, use: :slugged
 
-  belongs_to :main_tab
+  belongs_to :main_tab, optional: true
   has_many :requests, :as => :requestable
   has_rich_text :body
   set_sortable :sort
 
-  #validates :title, :presence => true
-  #validates :body, :presence => true
+  validates :title, :presence => true
+  validates :body, :presence => true
 
   def should_generate_new_friendly_id?
     title_changed?
