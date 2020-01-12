ab5c7affa4f9c6336b05a9245829df059c53ac79
Jacob <kanewers@gmail.com>
2020-01-12 14:18:05 +0100

    Moved preview modal show to preview.js.erb

--- app/javascript/src/adminpanel/requestable-ajax.js
+++ app/javascript/src/adminpanel/requestable-ajax.js
@@ -1,15 +1,3 @@
-var clicked = false;
-$('[id="preview-button"]').click(function () {
-    clicked = true;
-});
 $('#preview-modal').on('hidden.bs.modal', function () {
-    clicked = false;
     $('#preview-body').empty();
-});
-$('form').on('ajax:success', function (event, data, status, xhr) {
-    if (clicked) {
-        $('#preview-modal').modal('show');
-    }
-});
-$('form').on('ajax:error', function (event, data, status, xhr) {
 });
