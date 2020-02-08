e68bb1845bffbe4b9ad99e598e27a00ad7e4e294
Jacob <kanewers@gmail.com>
2020-02-08 12:21:08 +0100

    Added partner delete method

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -22,6 +22,18 @@ class PartnersController < RequestablesController
     end
   end
 
+  def destroy
+    @partner = Partner.find(params[:id])
+    if current_user.has_role?(:admin)
+      @partner.destroy
+    else
+      @request = Request.new(status: 1, user_id: current_user.id, action: "destroy",
+                             requestable_type: "Partner", requestable_id: @partner.id)
+      @request.save
+    end
+    redirect_to partners_path
+  end
+
   private def partner_params
     params.require(:partner).permit(:name, :link, :image)
   end
