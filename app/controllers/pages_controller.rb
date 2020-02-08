cd49bda4778db351be0953dc484159eacd24134a
Jacob <kanewers@gmail.com>
2020-02-08 12:21:33 +0100

    Added partners display on main page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,18 +4,11 @@ class PagesController < ApplicationController
   def main
     @main_tabs = MainTab.all
     @categories = Category.all
+    @partners = Partner.all
     @events_count = properEventsAmount
     @events = Event.where("status = 1").order("important DESC, created_at DESC").take(@events_count)
     @plan_tab = SubTab.where("title = ?", "Plan lekcji").take!
     @classes_tab = SubTab.where("title = ?", "Zajęcia pozalekcyjne").take!
-  end
-
-  def properEventsAmount
-    important = Event.where('important').all.count
-    if important > 3
-      return important + 9 - 3
-    end
-    9
   end
 
   def dashboard
@@ -28,5 +21,14 @@ class PagesController < ApplicationController
 
   def test
     render :layout => 'test'
+  end
+
+  private
+  def properEventsAmount
+    important = Event.where('important').all.count
+    if important > 3
+      return important + 9 - 3
+    end
+    9
   end
 end
