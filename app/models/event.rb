fc8e785dfbadc39b3fc0c6065efdfb005205ce74
Jacob <kanewers@gmail.com>
2020-02-09 22:24:08 +0100

    Added images compression for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+  after_commit :compress_images
   extend FriendlyId
   include Requestable
   friendly_id :title, use: :slugged
@@ -12,4 +13,39 @@ class Event < ApplicationRecord
   validates :title, :presence => true
   validates :body, :presence => true
   validates :image, :presence => true
+
+
+  def compress_images
+    if @commit
+      return true
+    end
+    @commit = false
+    if self.respond_to?(:image)
+      if self.image.attached?
+        self.image.blob.open do |file|
+          result = ImageProcessing::Vips.source(file).resize_to_limit!(4096, nil)
+          self.image.purge_later
+          @commit = true
+          self.image.attach(io: result, filename: File.basename(result), content_type: "application/image")
+          @commit = false
+        end
+      end
+    end
+    if self.respond_to?(:gallery_images)
+      if self.gallery_images.attached?
+        images = []
+        self.gallery_images.blobs.each do |blob|
+          blob.open do |file|
+            images << ImageProcessing::Vips.source(file).resize_to_limit!(4096, nil)
+          end
+        end
+        self.gallery_images.purge_later
+        images.each do |img|
+          @commit = true
+          gallery_images.attach(io: img, filename: File.basename(img), content_type: "application/image")
+          @commit = false
+        end
+      end
+    end
+  end
 end
