b876f1eb3ad3fdc8c848e98085ad4e1598e2e4fb
Jacob <kanewers@gmail.com>
2020-02-10 20:29:22 +0100

    Fixed images being compressed multiple times

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,31 +20,40 @@ class Event < ApplicationRecord
       return true
     end
     @commit = false
-    if self.respond_to?(:image)
-      if self.image.attached?
+    # If model responds to the attached image
+    if self.respond_to?(:image) and self.image.attached?
+      # and image was not yet compressed
+      unless self.image.blob.filename.base.start_with?("lite_")
+        # open the blob, compress image and attach it to the model
         self.image.blob.open do |file|
           result = ImageProcessing::Vips.source(file).resize_to_limit!(4096, nil)
           self.image.purge_later
           @commit = true
-          self.image.attach(io: result, filename: File.basename(result), content_type: "application/image")
+          self.image.attach(io: result, filename: "lite_" + File.basename(result), content_type: "application/image")
           @commit = false
         end
       end
+
     end
-    if self.respond_to?(:gallery_images)
-      if self.gallery_images.attached?
-        images = []
-        self.gallery_images.blobs.each do |blob|
+    # If model responds to the attached gallery
+    if self.respond_to?(:gallery_images) and self.gallery_images.attached?
+      images = []
+      # For each image in the gallery
+      self.gallery_images.blobs.each do |blob|
+        # compress the image if it was not yet compressed
+        unless blob.filename.base.start_with?("lite_")
           blob.open do |file|
             images << ImageProcessing::Vips.source(file).resize_to_limit!(4096, nil)
           end
         end
-        self.gallery_images.purge_later
-        images.each do |img|
-          @commit = true
-          gallery_images.attach(io: img, filename: File.basename(img), content_type: "application/image")
-          @commit = false
-        end
+      end
+      # purge old images
+      self.gallery_images.purge_later
+      # attach new images
+      images.each do |img|
+        @commit = true
+        gallery_images.attach(io: img, filename: File.basename(img), content_type: "application/image")
+        @commit = false
       end
     end
   end
