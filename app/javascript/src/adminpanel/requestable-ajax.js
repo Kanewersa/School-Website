2f6836a98282483cdd1cde0b2634d3213f70ff97
Jacob <kanewers@gmail.com>
2020-02-11 16:06:06 +0100

    Added loader when waiting for ajax

--- app/javascript/src/adminpanel/requestable-ajax.js
+++ app/javascript/src/adminpanel/requestable-ajax.js
@@ -1,3 +1,20 @@
 $('#preview-modal').on('hidden.bs.modal', function () {
     $('#preview-body').empty();
 });
+
+$(document).ready(function() {
+    var elementsArray = document.querySelectorAll(".loading");
+    if(elementsArray.length > 0)
+    {
+        elementsArray.forEach(function(elem) {
+            elem.addEventListener("click", function() {
+                //Show loader
+                $('#loader').modal({
+                    show: true,
+                    keyboard: false,
+                    backdrop: 'static'
+                });
+            });
+        });
+    }
+});
