4321c27311a8027356b132526416856619763a3f
Jacob <kanewers@gmail.com>
2020-02-11 10:24:18 +0100

    Fixed seeds to contain images and bodies

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-MainTab.create(id: 1, title: "O Szkole", slug: "o-szkole")
-MainTab.create(id: 2, title: "Organizacja", slug: "organizacja")
-MainTab.create(id: 4, title: "Dla uczniów", slug: "dla-uczniów")
-MainTab.create(id: 5, title: "Dla rodziców ", slug: "dla-rodziców")
-MainTab.create(id: 6, title: "Kontakt", slug: "kontakt")
+MainTab.create(id: 1, title: "O Szkole",
+               slug: "o-szkole",
+               body: "Tekst zakładki",
+               image: Rails.root.join("app/assets/images/image_not_available.png").open)
+MainTab.create(id: 2,
+               title: "Organizacja",
+               slug: "organizacja",
+               body: "Tekst zakładki",
+               image: Rails.root.join("app/assets/images/image_not_available.png").open)
+MainTab.create(id: 4,
+               title: "Dla uczniów",
+               slug: "dla-uczniów",
+               body: "Tekst zakładki",
+               image: Rails.root.join("app/assets/images/image_not_available.png").open)
+MainTab.create(id: 5,
+               title: "Dla rodziców",
+               slug: "dla-rodziców",
+               body: "Tekst zakładki",
+               image: Rails.root.join("app/assets/images/image_not_available.png").open)
+MainTab.create(id: 6,
+               title: "Kontakt",
+               slug: "kontakt",
+               body: "Tekst zakładki",
+               image: Rails.root.join("app/assets/images/image_not_available.png").open)
 
 Category.create(id: 1, title: "Ogólnoszkolne", slug: "ogolnoszkolne")
 Category.create(id: 2, title: "Oddział przedszkolny", slug: "oddzial-przedszkolny")
@@ -18,6 +37,12 @@ Category.create(id: 3, title: "Klasy I-III", slug: "klasy-i-iii")
 Category.create(id: 4, title: "Klasy IV-VIII", slug: "klasy-iv-viii")
 Category.create(id: 5, title: "Świetlica", slug: "swietlica")
 
-SubTab.create(title: "Plan lekcji", main_tab_id: 2, slug: "plan-lekcji", sort: 1, status: 1)
+SubTab.create(title: "Plan lekcji",
+              main_tab_id: 2,
+              slug: "plan-lekcji",
+              sort: 1,
+              status: 1,
+              body: "Tekst zakładki")
 
-Token.create(role: "admin", value: "value", name: "Administrator")
+Token.create(role: "admin", value: "value", name: "Administrator",
+             body: "Tekst zakładki")
