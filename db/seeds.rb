459f737c4923f781cacc753a4972a2fcbd10ecd7
Jacob <kanewers@gmail.com>
2020-02-11 12:52:14 +0100

    Fixed seeds to correctly seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,30 +6,67 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-MainTab.create(id: 1, title: "O Szkole",
-               slug: "o-szkole",
-               body: "Tekst zakładki",
-               image: Rails.root.join("app/assets/images/image_not_available.png").open)
-MainTab.create(id: 2,
-               title: "Organizacja",
-               slug: "organizacja",
-               body: "Tekst zakładki",
-               image: Rails.root.join("app/assets/images/image_not_available.png").open)
-MainTab.create(id: 4,
-               title: "Dla uczniów",
-               slug: "dla-uczniów",
-               body: "Tekst zakładki",
-               image: Rails.root.join("app/assets/images/image_not_available.png").open)
-MainTab.create(id: 5,
-               title: "Dla rodziców",
-               slug: "dla-rodziców",
-               body: "Tekst zakładki",
-               image: Rails.root.join("app/assets/images/image_not_available.png").open)
-MainTab.create(id: 6,
-               title: "Kontakt",
-               slug: "kontakt",
-               body: "Tekst zakładki",
-               image: Rails.root.join("app/assets/images/image_not_available.png").open)
+path = Rails.root.join("app/assets/images/image_not_available.png")
+tab = MainTab.new
+tab.title = "O Szkole"
+tab.body = "Tekst zakładki"
+tab.slug = "o-szkole"
+tab.image.attach(io: File.open(path), filename: "image_not_available.png", content_type: "application/image")
+tab.save
+
+tab = MainTab.new
+tab.body = "Tekst zakładki"
+tab.title = "Organizacja"
+tab.slug = "organizacja"
+tab.image.attach(io: File.open(path), filename: "image_not_available.png", content_type: "application/image")
+tab.save
+
+tab = MainTab.new
+tab.body = "Tekst zakładki"
+tab.title = "Dla uczniów"
+tab.slug = "dla-uczniow"
+tab.image.attach(io: File.open(path), filename: "image_not_available.png", content_type: "application/image")
+tab.save
+
+tab = MainTab.new
+tab.body = "Tekst zakładki"
+tab.title = "Dla rodziców"
+tab.slug = "dla-rodzicow"
+tab.image.attach(io: File.open(path), filename: "image_not_available.png", content_type: "application/image")
+tab.save
+
+tab = MainTab.new
+tab.body = "Tekst zakładki"
+tab.title = "Kontakt"
+tab.slug = "kontakt"
+tab.image.attach(io: File.open(path), filename: "image_not_available.png", content_type: "application/image")
+tab.save
+
+#
+#MainTab.create(id: 1, title: "O Szkole",
+#               slug: "o-szkole",
+#               body: "Tekst zakładki",
+#               image: Rails.root.join("app/assets/images/image_not_available.png").open)
+#MainTab.create(id: 2,
+#               title: "Organizacja",
+#               slug: "organizacja",
+#               body: "Tekst zakładki",
+#               image: Rails.root.join("app/assets/images/image_not_available.png").open)
+#MainTab.create(id: 4,
+#               title: "Dla uczniów",
+#               slug: "dla-uczniów",
+#               body: "Tekst zakładki",
+#               image: Rails.root.join("app/assets/images/image_not_available.png").open)
+#MainTab.create(id: 5,
+#               title: "Dla rodziców",
+#               slug: "dla-rodziców",
+#               body: "Tekst zakładki",
+#               image: Rails.root.join("app/assets/images/image_not_available.png").open)
+#MainTab.create(id: 6,
+#               title: "Kontakt",
+#               slug: "kontakt",
+#               body: "Tekst zakładki",
+#               image: Rails.root.join("app/assets/images/image_not_available.png").open)
 
 Category.create(id: 1, title: "Ogólnoszkolne", slug: "ogolnoszkolne")
 Category.create(id: 2, title: "Oddział przedszkolny", slug: "oddzial-przedszkolny")
@@ -38,11 +75,17 @@ Category.create(id: 4, title: "Klasy IV-VIII", slug: "klasy-iv-viii")
 Category.create(id: 5, title: "Świetlica", slug: "swietlica")
 
 SubTab.create(title: "Plan lekcji",
-              main_tab_id: 2,
+              main_tab: MainTab.where(:title => "Organizacja").take,
               slug: "plan-lekcji",
               sort: 1,
               status: 1,
               body: "Tekst zakładki")
 
-Token.create(role: "admin", value: "value", name: "Administrator",
-             body: "Tekst zakładki")
+SubTab.create(title: "Zajęcia pozalekcyjne",
+              main_tab: MainTab.where(:title => "Dla uczniów").take,
+              slug: "zajecia-pozalekcyjne",
+              sort: 1,
+              status: 1,
+              body: "Tekst zakładki")
+
+Token.create(role: "admin", value: "value", name: "Administrator")
