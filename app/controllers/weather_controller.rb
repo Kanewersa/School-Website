c15c08578a011aaa8407380e111b20f9278535fc
Jacob <kanewers@gmail.com>
2020-02-17 22:20:27 +0100

    Added basic weather logic

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,5 +4,38 @@ class WeatherController < ApplicationController
   def show
     @main_tabs = MainTab.all
     @categories = Category.all
+
+    #url = "https://www.weatherlink.com/embeddablePage/summaryData/68fa4f3ad9034a45bf73705968756fc1"
+    url = "https://www.weatherlink.com/embeddablePage/getData/68fa4f3ad9034a45bf73705968756fc1"
+    resp = Net::HTTP.get_response(URI.parse(url))
+    data = JSON.parse(resp.body)
+    set_current_weather(data)
+    # abort data.inspect
+  end
+
+
+  private def set_current_weather(data)
+    @overview = data["forecastOverview"][0]
+
+    ### Get current weather
+    @temperature = data["temperature"]
+    @humidity = data["humidity"]
+    @wind_speed = data["wind"]
+    #@wind_direction = @overview[5]["value"]
+    #@average_wind_speed_10min = @overview[7]["value"]
+    @barometer = data["barometer"]
+    @rain = data["rain"]
+    #@last_hour_rain = @overview[14]["value"]
+    @lowest_temperature = data["loTemp"]
+
+    @weather_code_morning = @overview["morning"]["weatherCode"]
+    @weather_code_afternoon = @overview["afternoon"]["weatherCode"]
+    @weather_code_evening = @overview["evening"]["weatherCode"]
+    @weather_code_night = @overview["night"]["weatherCode"]
+
+    @current_date = @overview["date"]
+    @day = Date.current.wday
+
+
   end
 end
--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -1,2 +1,97 @@
 module WeatherHelper
+
+  def get_day_name
+    @day += 1
+    @day_name = Date::DAYNAMES[(@day) % 7]
+  end
+
+  def get_time
+    hour = Time.now.hour
+    case hour
+    when 0..6
+      @weather_code = @weather_code_night
+    when 7..11
+      @weather_code = @weather_code_morning
+    when 12..16
+      @weather_code = @weather_code_afternoon
+    when 17..20
+      @weather_code = @weather_code_evening
+    else
+      @weather_code = @weather_code_night
+    end
+  end
+
+  def get_weather_icon
+    get_time
+    case @weather_code
+    when 116 # Partly cloudy
+      "<div class='icon icon-fit sun-shower'>
+        <div class='cloud'></div>
+        <div class='sun'>
+          <div class='rays'></div>
+        </div>
+      </div>".html_safe
+    when 1116 # Partly Cloudy
+      "<div class='icon icon-fit sun-shower'>
+        <div class='cloud'></div>
+        <div class='sun'>
+          <div class='rays'></div>
+        </div>
+      </div>".html_safe
+    when 119 # Cloudy
+      "<div class='icon icon-fit cloudy'>
+        <div class='cloud'></div>
+        <div class='cloud'></div>
+      </div>".html_safe
+    when 113 # Clear/Sunny
+      "<div class='icon icon-fit sunny'>
+        <div class='sun'>
+          <div class='rays'></div>
+        </div>
+      </div>".html_safe
+    when 1113 # Clear
+      "<div class='icon icon-fit sunny'>
+        <div class='sun'>
+          <div class='rays'></div>
+        </div>
+      </div>".html_safe
+    when 326 # Light snow
+      "<div class='icon icon-fit flurries'>
+        <div class='cloud'></div>
+        <div class='snow'>
+          <div class=f'lake'></div>
+          <div class='flake'></div>
+        </div>
+      </div>".html_safe
+    when 368 # Light snow showers
+      "<div class='icon icon-fit flurries'>
+        <div class='cloud'></div>
+        <div class='snow'>
+          <div class=f'lake'></div>
+          <div class='flake'></div>
+        </div>
+      </div>".html_safe
+    when 296 # Light rain
+      "<div class='icon icon-fit sun-shower'>
+        <div class='cloud'></div>
+        <div class='sun'>
+          <div class='rays'></div>
+        </div>
+        <div class='rain'></div>
+      </div>".html_safe
+    when 176 # Patchy rain nearby
+      "<div class='icon icon-fit sun-shower'>
+        <div class='cloud'></div>
+        <div class='sun'>
+          <div class='rays'></div>
+        </div>
+        <div class='rain'></div>
+      </div>".html_safe
+    else # Unknown weather
+      "<div class='icon icon-fit cloudy'>
+        <div class='cloud'></div>
+        <div class='cloud'></div>
+      </div>".html_safe
+    end
+  end
 end
